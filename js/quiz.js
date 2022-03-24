c34b7be38543049a9de49b20b979645166cc6204
rus19023 <rus19023@byui.edu>
2022-03-24 09:24:53 -0400

    move json folder

--- js/quiz.js
+++ js/quiz.js
@@ -2,8 +2,9 @@ const url = 'http://rus19023.github.io/myportfolio/330/exercises/questions.json'
 const url2 = 'https://gist.github.com/mariodev12/a923f2b651a005ca3ca7f851141efcbc';
 const url3 = 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/questions.json';
 const url4 = 'http://localhost/myportfolio/330/exercises/js/questions.json';
+const url6 = ''
 
-const url5 = JSON.stringify([
+const url5 = [
     { name: "Superman", realName: "Clark Kent" },
     { name: "Wonder Woman", realName: "Diana Prince" },
     { name: "Batman", realName: "Bruce Wayne" },
@@ -31,11 +32,11 @@ const url5 = JSON.stringify([
     { name: "The Hulk",realName: "Bruce Banner" },
     { name: "Spider-man",realName: "Peter Parker" },
     { name: "Cyclops",realName: "Scott Summers" }
-]);
+];
 
-console.log(url3);
+//console.log(url3);
 
-fetch(url3)
+fetch(url6)
 .then(res => res.json())
 .then(quiz => {
     view.start.addEventListener('click', () => game.start(quiz.questions), false);
@@ -69,8 +70,9 @@ const view = {
     hiScore: document.querySelector('#hiScore strong'),
 
     render(target, content, attributes) {
-        console.log(target + ' ' + content + ' ' + attributes);
+        //console.log('target: ' + target + ' content: ' + content + ' attributes: ' + attributes);
         for (const key in attributes) {
+            console.log('target: ' + target + ' content: ' + content + ' attributes: ' + attributes);
             target.setAttribute(key, attributes[key]);
         }
         target.innerHTML = content;
@@ -96,15 +98,15 @@ const view = {
     },
 
     buttons(array) {
-        console.log(array.map(value => `<button>${value}</button>`).join(''));
-        return array.map(value => `<button class="buttons">${value}</button>`).join('');
+        //console.log(array.map(value => `<button>${value}</button>`).join(''));
+        return array.map(value => `<button class="quizbutton">${value}</button>`).join('');
     },
 
     teardown() {
         this.hide(this.question);
         this.hide(this.response);
         this.show(this.start);
-        this.render(this.hiScore, game.hiScore(), '');
+        this.render(this.hiScore, game.hiScore(), {'class':'bghotpink'});
     },
 };
 
@@ -112,10 +114,15 @@ const game = {
     start(quiz) {
         console.log('start() invoked');
         this.score = 0;
+        this.bonus = 0;
+        this.isConsecutive = false;
+        this.consecutive = 0;
+        this.correct = 0;
+        this.cbonus = 0;
         this.questions = [...quiz];
         view.setup();
         this.ask();
-        this.secondsRemaining = 20;
+        this.secondsRemaining = quiz.length * 8;
         this.timer = setInterval( this.countdown , 1000 );
     },
 
@@ -125,11 +132,11 @@ const game = {
             shuffle(this.questions);
             this.question = this.questions.pop();
             const options = [this.questions[0].realName, this.questions[1].realName, this.questions[2].realName, this.question.realName];
-            console.log(options);
+            //console.log(options);
             shuffle(options);
             const question = `What is ${this.question.name}'s real name?`;
             view.render(view.question, question);
-            console.log(view.buttons(options));
+            //console.log(view.buttons(options));
             view.render(view.response, view.buttons(options), {'class':'buttonbox'});
         } else {
             this.gameOver();
@@ -140,12 +147,43 @@ const game = {
         console.log('check(event) invoked');
         const response = event.target.textContent;
         const answer = this.question.realName;
-        if(response === answer){
-        view.render(view.result,'Correct!',{'class':'correct'});
-        this.score++;
-        view.render(view.score,this.score);
+        if(response === answer) {
+            view.render(view.result,'Correct!',{'class':'correct'});
+            this.score += 10;
+            view.render(view.score, this.score);
+            this.correct++;
+            this.isConsecutive = true;
         } else {
-        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+            this.isConsecutive = false;
+            view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+        }
+        if (this.isConsecutive) {
+            this.consecutive++;
+            if (this.consecutive > 4) {
+                this.cbonus = 10;
+            } else if (this.consecutive > 9) {
+                this.cbonus = 20;
+            } else if (this.consecutive > 14) {
+                this.cbonus = 30;
+            } else if (this.consecutive > 19) {
+                this.cbonus = 40;
+            } else if (this.consecutive > 24) {
+                this.cbonus = 50;
+            } else if (this.consecutive > 39) {
+                this.cbonus = 75;
+            } else if (this.consecutive > 49) {
+                this.cbonus = 100;
+            }
+            this.bonus += (this.cbonus * this.consecutive);
+            console.log(`CBonus: ${this.cbonus}, consecutive: ${this.consecutive}, bonus: ${this.bonus}`);
+            console.log (`score: ${this.score}`);
+            this.score += this.bonus;
+        } else {
+            this.bonus += (this.cbonus * this.consecutive);
+            console.log(`CBonus: ${this.cbonus}, consecutive: ${this.consecutive}, bonus: ${this.bonus}`);
+            console.log (`score: ${this.score}`);
+            this.score += this.bonus;
+            this.consecutive = 0;
         }
         this.ask();
     },
@@ -172,7 +210,9 @@ const game = {
         const hi = localStorage.getItem('highScore') || 0;
         if(this.score > hi || hi === 0) {
             localStorage.setItem('highScore', this.score);
-            view.render(view.info, '** NEW HIGH SCORE! **');
+
+            // TODO: get username from db, add to high score display
+            view.render(view.info, '** NEW HIGH SCORE! **', {'class':'bghotpink'});
         }
         return localStorage.getItem('highScore');
     },
